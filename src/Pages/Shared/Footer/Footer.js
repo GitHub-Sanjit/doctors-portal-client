933ba063cabcb537f61bad9965454292f0328868
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-13 00:54:50 +0600

    71-9 Module Summary and set Footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,66 @@
 import React from "react";
+import footer from "../../../assets/images/footer.png";
 
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return (
+    <footer
+      style={{ background: `url(${footer})`, backgroundSize: "cover" }}
+      className=" p-10 "
+    >
+      <div className="footer">
+        <div>
+          <span className="footer-title">Services</span>
+          <a href="/" className="link link-hover">
+            Branding
+          </a>
+          <a href="/" className="link link-hover">
+            Design
+          </a>
+          <a href="/" className="link link-hover">
+            Marketing
+          </a>
+          <a href="/" className="link link-hover">
+            Advertisement
+          </a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a href="/" className="link link-hover">
+            About us
+          </a>
+          <a href="/" className="link link-hover">
+            Contact
+          </a>
+          <a href="/" className="link link-hover">
+            Jobs
+          </a>
+          <a href="/" className="link link-hover">
+            Press kit
+          </a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a href="/" className="link link-hover">
+            Terms of use
+          </a>
+          <a href="/" className="link link-hover">
+            Privacy policy
+          </a>
+          <a href="/" className="link link-hover">
+            Cookie policy
+          </a>
+        </div>
+      </div>
+      {/* <div>
+        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+      </div> */}
+      <footer className="footer mt-32 footer-center p-4  text-base-content">
+        <div>
+          <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+        </div>
+      </footer>
+    </footer>
+  );
 };
 
 export default Footer;
