7d148e6f0e4c25100717d94459eaa5864e5560d6
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-12 14:31:04 +0600

    Setup react router dom layout and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Routes/Routes/Routes";
 
 function App() {
   return (
     <div>
-      <button className="btn btn-outline">Button</button>
-      <button className="btn btn-outline btn-primary">Button</button>
-      <button className="btn btn-outline btn-secondary">Button</button>
-      {/* <button className="btn btn-outline btn-accent">Button</button> */}
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
