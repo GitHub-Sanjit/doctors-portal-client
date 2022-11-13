cf0d8801467eebf04eb474115fc7351ba2b57ef3
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-14 01:55:32 +0600

    73-9 Private Route and redirect user to the page they wanted to go

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -14,12 +14,15 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
@@ -28,18 +31,20 @@ const AuthProvider = ({ children }) => {
   };
 
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unSubscribe();
   }, []);
 
-  const authInfo = { createUser, signIn, user, logOut, updateUser };
+  const authInfo = { createUser, signIn, user, loading, logOut, updateUser };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
