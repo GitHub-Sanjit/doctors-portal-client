e201406d88dc9a6def14dea1bdd73307f43de810
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-17 19:26:32 +0600

    75-8 Create Make admin API limit the feature based on admin level

--- src/Pages/Dashboard/AllUsers/AllUsers.js
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import toast from "react-hot-toast";
 
 const AllUsers = () => {
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/users");
@@ -10,6 +11,23 @@ const AllUsers = () => {
       return data;
     },
   });
+
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PUT",
+      headers: {
+        authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          toast.success("Make admin successfully");
+          refetch();
+        }
+      });
+  };
 
   return (
     <div>
@@ -22,8 +40,8 @@ const AllUsers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
-              <th>Favorite Color</th>
-              <th>Favorite Color</th>
+              <th>Admin</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -32,8 +50,19 @@ const AllUsers = () => {
                 <th>{i + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>Blue</td>
-                <td>Blue</td>
+                <td>
+                  {user?.role !== "admin" && (
+                    <button
+                      onClick={() => handleMakeAdmin(user._id)}
+                      className="btn btn-xs btn-primary"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button className="btn btn-xs btn-warning">Delete</button>
+                </td>
               </tr>
             ))}
 
