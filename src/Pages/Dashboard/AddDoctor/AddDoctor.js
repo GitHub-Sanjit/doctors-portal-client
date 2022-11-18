6f86a8bec2454dda1f2dc3a919e26f94de72af12
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-18 21:31:41 +0600

    76-2 Load services with data project and Add file type input

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,9 +1,73 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddDoctor = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const handleAddDoctor = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
+    <div className="w-96 p-7">
       <h2 className="text-4xl">Add A Doctor</h2>
+      <form onSubmit={handleSubmit(handleAddDoctor)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="name"
+            {...register("name", {
+              required: "Name is required",
+            })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.name && (
+            <p className="text-red-600" role="alert">
+              {errors.name?.message}
+            </p>
+          )}
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Email Address is required",
+            })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.email && (
+            <p className="text-red-600" role="alert">
+              {errors.email?.message}
+            </p>
+          )}
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Specialty</span>
+          </label>
+          <select className="select select-bordered w-full max-w-xs">
+            <option defaultValue>Pick a Specialty</option>
+            <option>Teeth Orthodon</option>
+            <option>Greedo</option>
+          </select>
+
+          <br />
+        </div>
+        <input
+          className="btn btn-accent w-full"
+          value="Add Doctor"
+          type="submit"
+        />
+      </form>
     </div>
   );
 };
