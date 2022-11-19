c0dc137f5cf57f4c4bb3b0607d023fcfe169260c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-19 23:13:41 +0600

    77-4 Basic Stipe setup and with simple card form

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,5 +1,11 @@
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 
 const Payment = () => {
   const booking = useLoaderData();
